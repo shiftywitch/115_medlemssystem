2ac37c8b0229d9a52cb6a89b8cbc9ed6ffca38c6
Eirik <ethiles@online.no>
2021-09-22 13:22:21 +0200

    Fixup from template. Updated bootstrap version, and added config-method

---
+++ webdata/config.sample.php
@@ -0,0 +1,9 @@
+<?php 
+
+$config["db"]["host"] = "localhost";
+$config["db"]["user"] = "user";
+$config["db"]["pass"] = "password";
+$config["db"]["database"] = "database";
+
+$config["general"]["projectRoot"] = "";
+$config["general"]["pepper"] = "IAmBadAtSecurity";
--- webdata/init.php
+++ webdata/init.php
@@ -3,8 +3,32 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require 'connection.php';
 
-if($db->connect_error){
-    die("Connection failed: " . $blogdb->connect_error);
+require_once 'config.php';
+
+if(!isset($config) || empty($config)){
+    die("<h1>Configuration error</h1><p>Copy the webdata/config.sample.php file to webdata/config.php and fill out your connection settings.</p>");
+}
+
+$projectRoot = $config["general"]["projectRoot"];
+
+function getConfig($val, $group = "general"){
+    global $config;
+
+    if(isset($config[$group][ $val ])){
+        return $config[$group][$val];
+    }
+    return false;
+}
+
+function database(){
+    global $config;
+
+    $db = new mysqli($config["db"]["host"], $config["db"]["user"], $config["db"]["pass"], $config["db"]["database"]);
+
+    if($db->connect_error){
+        die("Connection failed: " . $db->connect_error);
+    }
+
+    return $db;
 }
